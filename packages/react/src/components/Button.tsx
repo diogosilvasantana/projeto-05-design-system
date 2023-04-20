b7a08a76f018415e5d496bea4b0fbbc3c8696508
Diogo Silva <diogo@MacBook-Pro-de-Diogo.local>
2023-04-19 21:08:26 -0300

    Componentes do Design System - Componente: Button

---
+++ packages/react/src/components/Button.tsx
@@ -0,0 +1,89 @@
+import { ComponentProps, ElementType } from 'react'
+import { styled } from '../styles'
+
+export const Button = styled('button', {
+  all: 'unset',
+  padding: '0 $4',
+  borderRadius: '$sm',
+  fontSize: '$sm',
+  fontWeight: '$medium',
+  fontFamily: '$default',
+  textAlign: 'center',
+  minWidth: 120,
+  boxSizing: 'border-box',
+
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  gap: '$2',
+
+  cursor: 'pointer',
+
+  '&:disabled': {
+    cursor: 'not-allowed',
+  },
+
+  svg: {
+    width: '$4',
+    height: '$4',
+  },
+
+  variants: {
+    variant: {
+      primary: {
+        color: '$white',
+        background: '$proart500',
+
+        '&:not(:disabled):hover': {
+          background: '$proart300',
+        },
+
+        '&:disabled': {
+          background: '$gray200',
+        },
+      },
+      secondary: {
+        color: '$proart300',
+        border: '2px solid $proart500',
+
+        '&:not(:disabled):hover': {
+          background: '$proart500',
+          color: '$white',
+        },
+
+        '&:disabled': {
+          color: '$gray200',
+          borderColor: '$gray200',
+        },
+      },
+      tertiary: {
+        color: '$gray100',
+
+        '&:not(:disabled):hover': {
+          color: '$white',
+        },
+
+        '&:disabled': {
+          color: '$gray600',
+        },
+      },
+    },
+    size: {
+      sm: {
+        height: 38,
+      },
+      md: {
+        height: 46,
+      },
+    },
+  },
+
+  defaultVariants: {
+    variant: 'primary',
+    size: 'md',
+  },
+})
+
+export interface ButtonProps extends ComponentProps<typeof Button> {
+  as?: ElementType
+}
